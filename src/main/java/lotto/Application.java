5934661443bafe84750db1b3121ccb144bc867c3
gamsayeon <aud4551@naver.com>
2025-11-03 15:25:49 +0900

    feat: 구입금액 입력 기능 구현

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,11 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        System.out.println("구입금액을 입력해 주세요.");
+        Integer purchaseAmount = Integer.parseInt(Console.readLine());
+
     }
 }
